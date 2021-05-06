b408aca79ba6446dae113b88e53fa903ace65465
AbhikS847 <abhikshrestha@gmail.com>
2021-05-06 21:49:44 +1000

    Added authentiaction wtih postman

--- server.js
+++ server.js
@@ -2,15 +2,27 @@ const express = require('express');
 
 const app = express();
 
+const connectDB = require('./config/db');
+
 const dotenv = require('dotenv');
 
 dotenv.config();
 
 const port = process.env.PORT || 5000;
 
+connectDB();
+
+app.use(express.json({extended:false}));
+
 app.get('/',(req,res)=>{
     res.send("Hello world");
 })
+
+app.use('/api/users',require('./routes/api/users'));
+app.use('/api/auth',require('./routes/api/auth'));
+app.use('/api/profile',require('./routes/api/profile'));
+app.use('/api/posts',require('./routes/api/posts'));
+
 
 app.listen(port, () => {console.log(`Server is running on port ${port}`)});
 
